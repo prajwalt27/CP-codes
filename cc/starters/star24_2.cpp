1bb6b6e5471ed173ebe2ac8227fc7737bd45d677
Prajwal Toundakar <81010237+prajwalt27@users.noreply.github.com>
2022-03-08 00:09:59 +0530

    Add files via upload

---
+++ cc/starters/star24_2.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vl vector<ll>
+#define vb vector<bool>
+#define pii pair<int,int>
+#define pb push_back
+#define sz(p) p.size()
+#define l(p)  p.length()
+#define all(p) p.begin(), p.end()
+#define lpi(i, a, b) for (int i = a; i < b; i++)
+#define lpd(i, a, b) for (int i = a; i >= b; i--)
+#define tst  int T; cin >> T; while (T--)
+#define fio   ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    fio
+    tst
+    {
+        int x,y;
+        cin>>x>>y;
+        int k=(x-y);
+        if(y==0)
+        {
+            cout<<x<<"\n";
+        }
+        else if(k==0)
+        {
+            cout<<y*2-1<<"\n";
+        }
+        else{
+              cout<<k+2*y<<"\n";
+        }
+
+        
+
+    }
+    return 0;
+}
