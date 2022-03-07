5ab2ab9c6952ef9fe5baed616d6cee3efc556970
Prajwal Toundakar <81010237+prajwalt27@users.noreply.github.com>
2022-03-07 20:21:05 +0530

    Create feb1.cpp

---
+++ cc/cookoof/feb1.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if((a+c)>=b)
+        {
+            cout<<(a+c)<<"\n";
+        }
+        else
+        {
+            cout<<b<<"\n";
+        }
+    }
+}
