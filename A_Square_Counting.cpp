43d61ff9e84829fc3d25996c23ffcac38f6f2dcc
Prajwal Toundakar <81010237+prajwalt27@users.noreply.github.com>
2022-03-07 20:12:10 +0530

    Add files via upload

---
+++ A_Square_Counting.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {
+      long long int n,s;
+      cin>>n>>s;
+        long long int p=(n*n);
+      cout<<s/p<<'\n';
+  }
+    return 0;
+}
